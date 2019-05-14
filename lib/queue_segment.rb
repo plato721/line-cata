4ed2668268569164318e9df77d11afbae3c31c12
Matt Ewell <matt.ewell@gmail.com>
2019-05-14 16:27:06 -0600

    Change total to offset
    This will let you get one segment, then determine position
    based on that, instead of also needing to know about the prior
    segment.

--- lib/queue_segment.rb
+++ lib/queue_segment.rb
@@ -1,18 +1,17 @@
 class QueueSegment
-  attr_reader :power_of_2, :segment_length, :overall_length
+  attr_reader :power_of_2, :segment_length, :offset
 
-  def initialize(power_of_2, segment_length, overall_length)
+  def initialize(power_of_2, segment_length, offset)
     @power_of_2 = power_of_2
     @segment_length = segment_length
-    @overall_length = overall_length
+    @offset = offset
   end
 
   def successor
-    next_segment_length = segment_length * 2
     self.class.new(
       power_of_2 + 1,
-      next_segment_length,
-      overall_length + next_segment_length
+      segment_length * 2,
+      offset + segment_length
     )
   end
 end
--- spec/queue_segment_spec.rb
+++ spec/queue_segment_spec.rb
@@ -4,18 +4,18 @@ RSpec.describe QueueSegment do
   it "has a power_of_2, a segment_total, and an overall_total" do
     power_of_2 = 0
     segment_length = 5
-    overall_length = 5
+    offset = 0 # represents the total length of previous segments
 
-    qs = described_class.new(power_of_2, segment_length, overall_length)
+    qs = described_class.new(power_of_2, segment_length, offset)
 
     expect(qs.power_of_2).to be_zero
     expect(qs.segment_length).to eq(5)
-    expect(qs.overall_length).to eq(5)
+    expect(qs.offset).to eq(0)
   end
 
   context "#successor" do
     let(:initial_segment){
-      described_class.new(0, 5, 5)
+      described_class.new(0, 5, 0)
     }
 
     it "has a successor" do
@@ -23,7 +23,7 @@ RSpec.describe QueueSegment do
 
       expect(result.power_of_2).to eq(1)
       expect(result.segment_length).to eq(10)
-      expect(result.overall_length).to eq(15)
+      expect(result.offset).to eq(5)
     end
 
     it "has another successor" do
@@ -31,7 +31,7 @@ RSpec.describe QueueSegment do
 
       expect(result.power_of_2).to eq(2)
       expect(result.segment_length).to eq(20)
-      expect(result.overall_length).to eq(35)
+      expect(result.offset).to eq(15)
     end
   end
 end
