a616c57ef0dab97adfcde0ea75d91d95d0d6bd89
Matt Ewell <matt.ewell@gmail.com>
2019-05-15 08:31:58 -0600

    Remove instance methods for Line
    No need for them with current requirements. Still
    use the QueueSegment as an object, but just use
    the Line class as single-use calculation module,
    which is how it was functioning anyway.

--- lib/line.rb
+++ lib/line.rb
@@ -1,31 +1,23 @@
 require './lib/queue_segment'
 
 class Line
-  attr_reader :list
+  class << self
+    def who_is_next(list, position)
+      queue_segment = build_segments_and_find(list.length,  position)
+      position_in_segment = queue_segment.relative_position(position)
+      power = queue_segment.power_of_2
 
-  def self.who_is_next(list, position)
-    new(list).who_is_next(position)
-  end
+      list_index = position_in_segment / 2**power
+      list[list_index]
+    end
 
-  def initialize(list)
-    @list = list
-  end
+    def build_segments_and_find(length, position)
+      current_segment = QueueSegment.new(0, length, 0)
+      loop do
+        return current_segment if current_segment.contains? position
 
-  def who_is_next(position)
-    queue_segment = build_segments_and_find(position)
-    position_in_segment = queue_segment.relative_position(position)
-    power = queue_segment.power_of_2
-
-    list_index = position_in_segment / 2**power
-    list[list_index]
-  end
-
-  def build_segments_and_find(position)
-    current_segment = QueueSegment.new(0, list.length, 0)
-    loop do
-      return current_segment if current_segment.contains? position
-
-      current_segment = current_segment.successor
+        current_segment = current_segment.successor
+      end
     end
   end
 end
